a32dc6eac3156bfcf864030c91f296c5ab525849
Adam Dai <adamdai97@gmail.com>
2020-12-01 12:26:03 -0800

    initial commit with barebones agent, planner, world, and superclass

---
+++ src/RMPS_superclass.m
@@ -0,0 +1,63 @@
+classdef RMPS_superclass < handle
+% RMPS_superclass
+%
+% General comments about the class go here.
+%
+% Usage:
+%   This should describe to construct an instance of this class,
+%   whereas the individual properties and methods should have
+%   their own docstrings (see below).
+%   
+%   instance = class_name(inputs)
+%
+% Properties:
+%   property_1 - description of property 1
+%   ...
+%   property_n - description of property n
+%   
+% Methods:
+%   method_1 - description of method 1
+%   ...
+%   method_n - description of method n
+%
+% Author/s: author name/s
+% Created: YYYY-MM-DD
+% Updated: YYYY-MM-DD
+
+% BEGIN CODE
+%% properties
+    properties
+        ...
+    end
+%% methods
+    methods
+        %% constructor
+        function self = class_name(inputs)
+        % self = class_name(inputs)
+        %
+        % See the class help text for how 
+            ...
+        end
+
+        %% other methods (it is helpful to break them up with cells)
+        function outputs = example_method(self,other_inputs)
+            % outputs = self.example_method(other_inputs)
+            %
+            % General comments about the method go here. Note that
+            % we will always use "self" to refer to this
+            %
+            % Usage:
+            %
+            % Inputs:
+            %
+            % Outputs:
+            %
+            % Examples:
+            %
+            % Author/s: author name/s
+            % Created: YYYY-MM-DD
+            % Updated: YYYY-MM-DD
+        end
+    end
+end
+% END CODE
